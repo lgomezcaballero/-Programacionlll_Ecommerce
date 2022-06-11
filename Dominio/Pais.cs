7a2e0e55e001aa8ea5767988a107ea2defdcdd2b
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-06-10 23:38:44 -0300

    Se agregan las clases Pais y Provincia a la capa de Dominio

---
+++ Dominio/Pais.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Pais
+    {
+        public int ID { get; set; }
+
+        public string NombrePais { get; set; }
+
+        public bool Estado { get; set; }
+
+    }
+}
---
+++ Dominio/Provincia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Provincia
+    {
+        public int ID { get; set; }
+
+        public string NombreProvincia { get; set; }
+
+        public string Pais { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
