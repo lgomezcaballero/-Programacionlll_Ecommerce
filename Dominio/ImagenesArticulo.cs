fb218bb0a0cf80977bfe45f143e22c6159aca1f2
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-06-10 23:32:10 -0300

    Se agregan las clases Contacto y ImagenesArticulo a la capa Dominio

---
+++ Dominio/ImagenesArticulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class ImagenesArticulo
+    {
+        public int ID { get; set; }
+
+        public int IDArticulo { get; set; }
+
+        public string URLImagen { get; set; }
+    }
+}
