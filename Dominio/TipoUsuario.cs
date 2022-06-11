a8de7905462606c7e396d057aa03d80fdd9c59f2
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-06-10 23:41:49 -0300

    Se agregan las clases TipoUsuario y Usuario a la capa de Dominio

---
+++ Dominio/TipoUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class TipoUsuario
+    {
+        public int ID { get; set; }
+
+        public string NombreTipo { get; set; }
+    }
+}
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Usuario
+    {
+        public int ID { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public int DNI { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Contraseña { get; set; }
+        public int Contacto { get; set; }
+
+        public int IDLocalidad { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
