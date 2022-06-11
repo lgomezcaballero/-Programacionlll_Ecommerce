9a30e44d2b0de4131d22cfdf75b46eb13cd4d667
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-06-10 23:35:28 -0300

    Se agregan las clases Localidad y Marca

---
+++ Dominio/Localidad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Localidad
+    {
+        public int ID { get; set; }
+
+        public int CodigoPostal { get; set; }
+
+        public string Provincia { get; set; }
+
+        public string NombreLocalidad { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
---
+++ Dominio/Marca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Marca
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+        public bool Estado { get; set; }
+    }
+}
