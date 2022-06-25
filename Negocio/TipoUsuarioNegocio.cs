1546bc0a592b05ca2c42f6efa987a245373bd356
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-06-25 00:34:07 -0300

    Se agrega la clase TipoUsuarioNegocio a la capa Negocio

---
+++ Negocio/TipoUsuarioNegocio.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using Negocio;
+
+namespace Negocio
+{
+    public class TipoUsuarioNegocio
+    {
+        public List<TipoUsuario> listar()
+        {
+            List<TipoUsuario> lista = new List<TipoUsuario>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsultaSP("SP_ListarMarcas");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    TipoUsuario aux = new TipoUsuario();
+                    if (!(datos.Lector["ID"] is DBNull))
+                        aux.ID = (byte)datos.Lector["ID"];
+                    if (!(datos.Lector["NombreTipo"] is DBNull))
+                        aux.NombreTipo = (string)datos.Lector["NombreTipo"];
+                    if (!(datos.Lector["Estado"] is DBNull))
+                        aux.Estado = (bool)datos.Lector["Estado"];
+
+
+                    lista.Add(aux);
+
+                }
+
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }//Falta probarlo
+
+        public void agregarTipoUsuario(TipoUsuario Tipo) // Falta probar
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsultaSP("SP_AgregarTipoUsuario");
+                datos.setParametros("@NombreTipo", Tipo.NombreTipo);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificarTipoUsuario(TipoUsuario Tipo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsultaSP("SP_ModificarTipoUsuario");
+                datos.setParametros("@ID", Tipo.ID);
+                datos.setParametros("@NombreTipo", Tipo.NombreTipo);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }//Falta probar
+
+        public void eliminarTipoUsuario(byte ID)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsultaSP("SP_EliminarTipoUsuario");
+                datos.setParametros("@ID", ID);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        } //Falta probar
+
+    }
+}
