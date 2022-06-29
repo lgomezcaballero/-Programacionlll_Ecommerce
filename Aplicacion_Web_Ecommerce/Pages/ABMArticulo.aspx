ef7f98936f07dbf3bd29afe5897c65da72c8051a
lgomezcaballero <leandrogomez343@gmail.com>
2022-06-29 19:49:16 -0300

    Se modifican botones abm

--- Aplicacion_Web_Ecommerce/Pages/ABMArticulo.aspx
+++ Aplicacion_Web_Ecommerce/Pages/ABMArticulo.aspx
@@ -1,4 +1,25 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Ecommerce.Master" AutoEventWireup="true" CodeBehind="ABMArticulo.aspx.cs" Inherits="Aplicacion_Web_Ecommerce.Pages.ModificarArticulo" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <%if (tipo.Equals("a"))
+        { %>
+        
+        
+        
+    <asp:Button Text="agregar" runat="server" />
+        
+        
+        
+        
+        
+        <%} %>
+    <%else { %>
+        
+        
+    <asp:Button Text="otra cosa" runat="server" />
+        
+        
+        
+        
+        <%} %>
 
 </asp:Content>
--- Aplicacion_Web_Ecommerce/Pages/ABMArticulo.aspx.cs
+++ Aplicacion_Web_Ecommerce/Pages/ABMArticulo.aspx.cs
@@ -9,17 +9,18 @@ namespace Aplicacion_Web_Ecommerce.Pages
 {
     public partial class ModificarArticulo : System.Web.UI.Page
     {
+        public int id;
+        public string tipo;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if(Request.QueryString["ID"] !=null && Request.QueryString["Type"] != null)
             {
-                string type = Request.QueryString["Type"];
+                tipo = Request.QueryString["Type"];
                 long id = long.Parse(Request.QueryString["ID"]);
-                if(type == "e")
-                {
-                    
-                }
             }
+            if (Request.QueryString["Type"] == "a")
+                tipo = Request.QueryString["Type"];
 
         }
     }
