bc4218b4b8555fff79dfcaf6e4a0fe1884564801
lgomezcaballero <leandrogomez343@gmail.com>
2022-07-03 15:32:03 -0300

    Se agrega funcionalidad a botón AgregarArticulo

--- Aplicacion_Web_Ecommerce/Pages/ABMArticulo.aspx.cs
+++ Aplicacion_Web_Ecommerce/Pages/ABMArticulo.aspx.cs
@@ -23,48 +23,59 @@ namespace Aplicacion_Web_Ecommerce.Pages
             {
 
                 MarcaNegocio mNegocio = new MarcaNegocio();
-                ddlMarcas.DataSource = mNegocio.listar();
-                ddlMarcas.DataValueField = "ID";
-                ddlMarcas.DataTextField = "Nombre";
-                ddlMarcas.DataBind();
+
                 CategoriaNegocio cNegocio = new CategoriaNegocio();
-                ddlCategorias.DataSource = cNegocio.listar();
-                ddlCategorias.DataValueField = "ID";
-                ddlCategorias.DataTextField = "Nombre";
-                ddlCategorias.DataBind();
+
                 GeneroNegocio gNegocio = new GeneroNegocio();
-                ddlGeneros.DataSource = gNegocio.listar();
-                ddlGeneros.DataValueField = "ID";
-                ddlGeneros.DataTextField = "Nombre";
-                ddlGeneros.DataBind();
+
+
                 Session.Add("listaMarcas", mNegocio.listar());
                 Session.Add("listaCategorias", cNegocio.listar());
                 Session.Add("listaGeneros", gNegocio.listar());
-                if (Request.QueryString["ID"] != null && Request.QueryString["Type"] != null)
+                if (Request.QueryString["Type"] != null)
                 {
                     tipo = Request.QueryString["Type"];
-                    id = long.Parse(Request.QueryString["ID"]);
-                }
-                if (Request.QueryString["Type"] == "a")
-                    tipo = Request.QueryString["Type"];
-                if (tipo == "e")
-                {
+                    if (Request.QueryString["ID"] != null)
+                        id = long.Parse(Request.QueryString["ID"]);
+
                     ArticuloTallaNegocio atNegocio = new ArticuloTallaNegocio();
-                    //Con esta funciom obtine el articulo que se busca
-                    articulo = obtenerArticulo(id);
                     articulo.Talles = new List<ArticuloTalla>();
                     articulo.Talles = atNegocio.listar(id);
                     Session.Add("tallesArticulo", articulo.Talles);
-                    //Esto lo que hace es precargar los datos con el articulo que se quiere modificar
-                    setearCamposModificar(articulo);
-                }
-
-                if(Request.QueryString["Type"] == "d") 
-                {
-                ArticuloNegocio articuloNegocio = new ArticuloNegocio();    
-                articuloNegocio.eliminarArticulo(id);
-                Response.Redirect("HomeAdmin.aspx", false);
-                
+                    if (tipo == "a")
+                    {
+                        a_ddlMarcas.DataSource = mNegocio.listar();
+                        a_ddlMarcas.DataValueField = "ID";
+                        a_ddlMarcas.DataTextField = "Nombre";
+                        a_ddlMarcas.DataBind();
+                        a_ddlCategorias.DataSource = cNegocio.listar();
+                        a_ddlCategorias.DataValueField = "ID";
+                        a_ddlCategorias.DataTextField = "Nombre";
+                        a_ddlCategorias.DataBind();
+                        a_ddlGeneros.DataSource = gNegocio.listar();
+                        a_ddlGeneros.DataValueField = "ID";
+                        a_ddlGeneros.DataTextField = "Nombre";
+                        a_ddlGeneros.DataBind();
+                    }
+                    if (tipo == "e")
+                    {
+                        ddlMarcas.DataSource = mNegocio.listar();
+                        ddlMarcas.DataValueField = "ID";
+                        ddlMarcas.DataTextField = "Nombre";
+                        ddlMarcas.DataBind();
+                        ddlCategorias.DataSource = cNegocio.listar();
+                        ddlCategorias.DataValueField = "ID";
+                        ddlCategorias.DataTextField = "Nombre";
+                        ddlCategorias.DataBind();
+                        ddlGeneros.DataSource = gNegocio.listar();
+                        ddlGeneros.DataValueField = "ID";
+                        ddlGeneros.DataTextField = "Nombre";
+                        ddlGeneros.DataBind();
+                        //Con esta funciom obtine el articulo que se busca
+                        articulo = obtenerArticulo(id);
+                        //Esto lo que hace es precargar los datos con el articulo que se quiere modificar
+                        setearCamposModificar(articulo);
+                    }
                 }
             }
         }
@@ -74,7 +85,14 @@ namespace Aplicacion_Web_Ecommerce.Pages
             Articulo aux = new Articulo();
             lista = (List<Articulo>)Session["listaArticulos"];
 
+            ArticuloTallaNegocio atNegocio = new ArticuloTallaNegocio();
+            ImagenesArticuloNegocio imgNegocio = new ImagenesArticuloNegocio();
+            Session.Add("imgsArticulo", imgNegocio.listar(id));
             aux = lista.Find(x => x.ID == idArticulo);
+            aux.Talles = new List<ArticuloTalla>();
+            aux.Talles = atNegocio.listar(idArticulo);
+            aux.Imagenes = new List<ImagenesArticulo>();
+            aux.Imagenes = imgNegocio.listar(id);
 
             return aux;
         }
@@ -86,7 +104,11 @@ namespace Aplicacion_Web_Ecommerce.Pages
             ddlCategorias.SelectedValue = articulo.Categoria.ID.ToString();
             ddlGeneros.SelectedValue = articulo.Genero.ID.ToString();
             e_txtPrecio.Text = articulo.Precio.ToString();
+            e_txtURLImagen.Text = articulo.Imagenes[0].URLImagen;
             e_txtDescripcion.Text = articulo.Descripcion;
+
+            //articulo.Talles = new List<ArticuloTalla>();
+            //articulo.Talles = (List<ArticuloTalla>)Session["tallesArticulo"];
             e_txtStockTallaXS.Text = articulo.Talles[0].Stock.ToString();
             e_txtStockTallaS.Text = articulo.Talles[1].Stock.ToString();
             e_txtStockTallaM.Text = articulo.Talles[2].Stock.ToString();
@@ -130,6 +152,9 @@ namespace Aplicacion_Web_Ecommerce.Pages
             articulo.Genero = new Genero();
             articulo.Genero.ID = byte.Parse(ddlGeneros.SelectedItem.Value);
             articulo.Nombre = e_txtNombre.Text;
+            articulo.Imagenes = new List<ImagenesArticulo>();
+            articulo.Imagenes = (List<ImagenesArticulo>)Session["imgsArticulo"];
+            articulo.Imagenes[0].URLImagen = e_txtURLImagen.Text;
             articulo.Descripcion = e_txtDescripcion.Text;
             articulo.Precio = decimal.Parse(e_txtPrecio.Text);
             articulo.Talles = new List<ArticuloTalla>();
@@ -163,133 +188,73 @@ namespace Aplicacion_Web_Ecommerce.Pages
             Response.Redirect("HomeAdmin.aspx");
         }
 
-
-
-        protected void BtbAgregarArticulo_Click(object sender, EventArgs e)
+        protected void a_btnAtrás_Click(object sender, EventArgs e)
         {
-            Articulo articulo = new Articulo();
-            articulo.Nombre = TextBoxNombreArticulo.Text;
-            articulo.Marca = new Marca();
-            //Aca capturo el nombre de la marca que se ingresa por teclado
-            articulo.Marca.Nombre = (TextBoxNombreMarcaArticulo.Text);
-            articulo.Categoria = new Categoria();
-            //Aca capturo el nombre de la categoria que se ingresa por teclado
-            articulo.Categoria.Nombre = TextBoxNombreCategoria.Text;
-            articulo.Genero = new Genero();
-            //Aca capturo el nombre del genero que se ingresa por teclado
-            articulo.Genero.Nombre = TextBoxGenero.Text;
-            articulo.Descripcion = TextBoxDescripcion.Text;
-            //articulo.Precio = decimal.Parse(TextBoxPrecio.Text);
-            //Esto es temporal, hasta que vea como usar el datos de la textbox
-            articulo.Precio = 2;
-            //articulo.Talla = new List<Talla>();
-            List<Talla> lista = new List<Talla>();
-            //Todo esto es temporal, lo uso para cargar el articulo
-            //Talla talla = new Talla();
-            //Esto se lo dejo asi por ahora
+            Response.Redirect("HomeAdmin.aspx", false);
+        }
 
-            //talla.IDTalla = 1;
-            //talla.Nombre = "XS";
-            //talla.Stock = 4;
-            //lista.Add(talla);
-
-
-
-            //Esto esta ok
-            Marca marca = new Marca();
-            marca = BuscarMarcaPorNombre(articulo.Marca.Nombre);
-            //Aca le pongo el id de la marca que encuentra la funcion
-            articulo.Marca.ID = marca.ID;
-
-            //Esto esta ok
-            Genero genero = new Genero();
-            genero = BuscarGeneroPorNombre(articulo.Genero.Nombre);
-            //Aca le pongo el id del Genero que encuentra la funcion
-            articulo.Genero.ID = genero.ID;
-
-
-            //Esto esta ok
-            Categoria categoria = new Categoria();
-            categoria = BuscarCategoriaPorNombre(articulo.Categoria.Nombre);
-            //Aca le pongo el id de la categoria que encuentra la funcion
-            articulo.Categoria.ID = categoria.ID;
-
-
-            //Esto lo hice asi de manera temporal
-            Talla talla = new Talla();
-            talla = BuscarTallaPorNombre(TextBoxNombreTalla.Text);
-            lista.Add(talla);
-
-
-
+        protected void a_btnCrear_Click(object sender, EventArgs e)
+        {
             ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+            Articulo articulo = new Articulo();
+
+            articulo.Marca = new Marca();
+            articulo.Marca.ID = short.Parse(a_ddlMarcas.SelectedItem.Value);
+            articulo.Categoria = new Categoria();
+            articulo.Categoria.ID = short.Parse(a_ddlCategorias.SelectedItem.Value);
+            articulo.Genero = new Genero();
+            articulo.Genero.ID = byte.Parse(a_ddlGeneros.SelectedItem.Value);
+            articulo.Nombre = a_txtNombre.Text;
+            articulo.Imagenes = new List<ImagenesArticulo>();
+            ImagenesArticulo img = new ImagenesArticulo();
+            img.URLImagen = a_txtUrlImagen.Text;
+            articulo.Imagenes.Add(img);
+            articulo.Descripcion = a_txtDescripcion.Text;
+            articulo.Precio = decimal.Parse(a_txtPrecio.Text);
+            articulo.Talles = new List<ArticuloTalla>();
+            articulo.Talles = setTallesArticulo();
+            articulo.Talles[0].Stock = a_txtStockTallaXS.Text != "" ? long.Parse(a_txtStockTallaXS.Text) : 0;
+            articulo.Talles[1].Stock = a_txtStockTallaS.Text != "" ? long.Parse(a_txtStockTallaS.Text) : 0;
+            articulo.Talles[2].Stock = a_txtStockTallaM.Text != "" ? long.Parse(a_txtStockTallaM.Text) : 0;
+            articulo.Talles[3].Stock = a_txtStockTallaL.Text != "" ? long.Parse(a_txtStockTallaL.Text) : 0;
+            articulo.Talles[4].Stock = a_txtStockTallaXL.Text != "" ? long.Parse(a_txtStockTallaXL.Text) : 0;
+            articulo.Talles[5].Stock = a_txtStockTallaXXL.Text != "" ? long.Parse(a_txtStockTallaXXL.Text) : 0;
+            articulo.Talles[6].Stock = a_txtStockTallaXXXL.Text != "" ? long.Parse(a_txtStockTallaXXXL.Text) : 0;
+            articulo.Talles[7].Stock = a_txtStockTalla30.Text != "" ? long.Parse(a_txtStockTalla30.Text) : 0;
+            articulo.Talles[8].Stock = a_txtStockTalla31.Text != "" ? long.Parse(a_txtStockTalla31.Text) : 0;
+            articulo.Talles[9].Stock = a_txtStockTalla32.Text != "" ? long.Parse(a_txtStockTalla32.Text) : 0;
+            articulo.Talles[10].Stock = a_txtStockTalla33.Text != "" ? long.Parse(a_txtStockTalla33.Text) : 0;
+            articulo.Talles[11].Stock = a_txtStockTalla34.Text != "" ? long.Parse(a_txtStockTalla34.Text) : 0;
+            articulo.Talles[12].Stock = a_txtStockTalla35.Text != "" ? long.Parse(a_txtStockTalla35.Text) : 0;
+            articulo.Talles[13].Stock = a_txtStockTalla36.Text != "" ? long.Parse(a_txtStockTalla36.Text) : 0;
+            articulo.Talles[14].Stock = a_txtStockTalla37.Text != "" ? long.Parse(a_txtStockTalla37.Text) : 0;
+            articulo.Talles[15].Stock = a_txtStockTalla38.Text != "" ? long.Parse(a_txtStockTalla38.Text) : 0;
+            articulo.Talles[16].Stock = a_txtStockTalla39.Text != "" ? long.Parse(a_txtStockTalla39.Text) : 0;
+            articulo.Talles[17].Stock = a_txtStockTalla40.Text != "" ? long.Parse(a_txtStockTalla40.Text) : 0;
+            articulo.Talles[18].Stock = a_txtStockTalla41.Text != "" ? long.Parse(a_txtStockTalla41.Text) : 0;
+            articulo.Talles[19].Stock = a_txtStockTalla42.Text != "" ? long.Parse(a_txtStockTalla42.Text) : 0;
+            articulo.Talles[20].Stock = a_txtStockTalla43.Text != "" ? long.Parse(a_txtStockTalla43.Text) : 0;
+            articulo.Talles[21].Stock = a_txtStockTalla44.Text != "" ? long.Parse(a_txtStockTalla44.Text) : 0;
+            articulo.Talles[22].Stock = a_txtStockTalla45.Text != "" ? long.Parse(a_txtStockTalla45.Text) : 0;
+            articulo.Talles[23].Stock = a_txtStockTalla46.Text != "" ? long.Parse(a_txtStockTalla46.Text) : 0;
+
             articuloNegocio.agregarArticulo(articulo);
             Response.Redirect("HomeAdmin.aspx", false);
-
-
-
         }
 
-        // Estas funciones son para poder ingresar el nombre de las entidades y no ingresar sis ID
-
-        protected Categoria BuscarCategoriaPorNombre(string NombreCategoria)
+        protected List<ArticuloTalla> setTallesArticulo()
         {
-
-            Categoria categoria = new Categoria();
-            List<Categoria> lista = new List<Categoria>();
-            CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
-            lista = categoriaNegocio.listar();
-
-            categoria = lista.Find(c => c.Nombre == NombreCategoria);
-
-
-            return categoria;
+            List<ArticuloTalla> lista = new List<ArticuloTalla>();
+            //ArticuloTallaNegocio atNegocio = new ArticuloTallaNegocio();
+            //long idart = idArticulo;
+            for (int i = 0; i < 24; i++)
+            {
+                ArticuloTalla aux = new ArticuloTalla();
+                //aux.IDArticulo = idArticulo;
+                aux.IDTalla = (byte)(i + 1);
+                lista.Add(aux);
+            }
+            return lista;
         }
-
-
-        protected Marca BuscarMarcaPorNombre(string NombreMarca)
-        {
-
-            Marca marca = new Marca();
-            List<Marca> lista = new List<Marca>();
-            MarcaNegocio marcaNegocio = new MarcaNegocio();
-            lista = marcaNegocio.listar();
-
-            marca = lista.Find(c => c.Nombre == NombreMarca);
-
-
-            return marca;
-        }
-
-
-        protected Genero BuscarGeneroPorNombre(string NombreGenero)
-        {
-
-            Genero genero = new Genero();
-            List<Genero> lista = new List<Genero>();
-            GeneroNegocio generoNegocio = new GeneroNegocio();
-            lista = generoNegocio.listar();
-
-            genero = lista.Find(c => c.Nombre == NombreGenero);
-
-
-            return genero;
-        }
-
-
-        protected Talla BuscarTallaPorNombre(string NombreTalla)
-        {
-
-            Talla talla = new Talla();
-            List<Talla> lista = new List<Talla>();
-            TallaNegocio tallaNegocio = new TallaNegocio();
-            lista = tallaNegocio.listar();
-
-            talla = lista.Find(c => c.Nombre == NombreTalla);
-
-
-            return talla;
-        }
-
     }
 }
