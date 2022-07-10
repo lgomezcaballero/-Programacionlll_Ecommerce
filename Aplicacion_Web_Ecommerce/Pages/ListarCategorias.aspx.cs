5e7516f12438f5f3c35e3161ed8345b09c6157a8
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-07-10 17:59:26 -0300

    Se agrega la pagina ListarCategorias

---
+++ Aplicacion_Web_Ecommerce/Pages/ListarCategorias.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
+
+namespace Aplicacion_Web_Ecommerce.Pages
+{
+    public partial class ListarCategorias : System.Web.UI.Page
+    {
+        //Esto lo hago para el abm de provincias
+        public List<Categoria> listacategoria = new List<Categoria>(); //ok
+        CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            listacategoria = categoriaNegocio.listar();
+            if (!IsPostBack)
+                Session.Add("listacategoria", listacategoria);
+
+        }
+    }
+}
