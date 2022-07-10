53ac917f028634562b4d2b97475a3a89189c2be9
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-07-10 18:04:03 -0300

    Se agrega la pagina ListarPais

---
+++ Aplicacion_Web_Ecommerce/Pages/ListarPaises.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
+
+namespace Aplicacion_Web_Ecommerce.Pages
+{
+    public partial class ListarPaises : System.Web.UI.Page
+    {
+
+        //Esto es para el abm de paises
+        public List<Pais> listaPaises = new List<Pais>(); //ok
+        PaisNegocio paisNegocio = new PaisNegocio();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //Esto lo hago para guardar el pais en session 
+            listaPaises = paisNegocio.listar();
+            if (!IsPostBack)
+                Session.Add("listapaises", listaPaises);
+        }
+    }
+}
