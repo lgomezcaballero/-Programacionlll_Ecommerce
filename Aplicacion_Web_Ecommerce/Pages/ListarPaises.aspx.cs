d3729c9b59aac534d318974ffbd8bf1b3b478bce
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-07-13 19:56:57 -0300

    Se agrega nivel de acceso a la pagina ListarPaises

--- Aplicacion_Web_Ecommerce/Pages/ListarPaises.aspx.cs
+++ Aplicacion_Web_Ecommerce/Pages/ListarPaises.aspx.cs
@@ -18,10 +18,29 @@ namespace Aplicacion_Web_Ecommerce.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            //Esto lo hago para guardar el pais en session 
-            listaPaises = paisNegocio.listar();
-            if (!IsPostBack)
-                Session.Add("listapaises", listaPaises);
+            if (Session["TeLogueaste"] == null)
+            {
+                Session.Add("error", "Debe loguearse para acceder a esta pagina");
+                Response.Redirect("ErrorLogin.aspx", false);
+            }
+
+            else
+            {
+                if (Session["Admin"] == null)
+                {
+                    Session.Add("error", "solo los administradores pueden acceder a esta pagina");
+                    Response.Redirect("ErrorAcceso.aspx", false);
+                }
+
+                else
+                {
+                    //Esto lo hago para guardar el pais en session 
+                    listaPaises = paisNegocio.listar();
+                    if (!IsPostBack)
+                        Session.Add("listapaises", listaPaises);
+                }
+            }
+
         }
     }
 }
